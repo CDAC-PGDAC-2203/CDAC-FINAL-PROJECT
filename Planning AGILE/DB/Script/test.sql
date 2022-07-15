528ea2c5d138e7befc25d7722d03cb1562b026c5
Hardik Agarwal <hardikagarwal1703@gmail.com>
2022-07-15 23:43:33 +0530

    User db and login screen done

---
+++ Planning AGILE/DB/Script/test.sql
@@ -0,0 +1,7 @@
+create table user(name varchar(50));
+
+insert into user values("Hardik");
+insert into user values("Kamana");
+insert into user values("Udit");
+insert into user values("Joy");
+insert into user values("Mayank");
