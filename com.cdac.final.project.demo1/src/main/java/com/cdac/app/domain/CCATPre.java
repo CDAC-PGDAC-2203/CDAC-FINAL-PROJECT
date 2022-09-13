dbcf016e82d0d83576f1069ca242b767519a44c7
Hardik Agarwal <hardikagarwal1703@gmail.com>
2022-09-13 13:05:32 +0530

    [Hardik] Added CCATPre Pojo

---
+++ com.cdac.final.project.demo1/src/main/java/com/cdac/app/domain/CCATPre.java
@@ -0,0 +1,36 @@
+package com.cdac.app.domain;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "ccat_pre")
+@Getter
+@Setter
+@ToString
+public class CCATPre {
+
+	@Id
+	@Column(name = "ccat_no")
+	private Integer ccatNo;
+
+	@Column(name = "f_name")
+	private String fName;
+
+	@Column(name = "m_name")
+	private String mName;
+
+	@Column(name = "l_name")
+	private String lName;
+
+	@Column(name = "dob")
+	private LocalDate dob;
+}
--- com.cdac.final.project.demo1/src/main/java/com/cdac/app/domain/CcatDomain.java
+++
@@ -1,5 +0,0 @@
-package com.cdac.app.domain;
-
-public class CcatDomain {
-
-}
