1a58eb6e2fb7c468fe27a954288baa4778289f28
mayanksachan07 <mayanksachan0703@gmail.com>
2022-09-13 18:07:29 +0530

    [mayank] added const

--- com.cdac.final.project.demo1/src/main/java/com/cdac/app/domain/PersonalDetails.java
+++ com.cdac.final.project.demo1/src/main/java/com/cdac/app/domain/PersonalDetails.java
@@ -12,7 +12,9 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -21,6 +23,8 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class PersonalDetails {
 
 	@Id
