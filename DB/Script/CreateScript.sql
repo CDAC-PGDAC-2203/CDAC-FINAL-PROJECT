917d7c0f283e480f7f41ead487587f0f292074ee
Hardik Agarwal <hardikagarwal1703@gmail.com>
2022-09-13 11:54:19 +0530

    [Hardik] First Commit
    Added project of Spring Boot, React as well as CreateScript.

---
+++ DB/Script/CreateScript.sql
@@ -0,0 +1,12 @@
+create database cdac_app_final;
+use cdac_app_final;
+
+-- Date format YYYY-MM-DD 
+create table if not exists ccat_pre(ccat_no bigint PRIMARY KEY NOT NULL, f_name varchar(25), 
+m_name varchar(25), l_name varchar(25), dob date);
+
+insert into ccat_pre values('2203001','Hardik',NULL,'Agarwal','2000-03-17');
+insert into ccat_pre values('2203002','Joy',NULL,'Pahari','1998-09-01');
+insert into ccat_pre values('2203003','Udit',NULL,'Keskar','1999-11-23');
+insert into ccat_pre values('2203004','Mayank',NULL,'Sachan','1994-03-07');
+insert into ccat_pre values('2203005','Kamana',NULL,'Mathur','1997-07-04');
--- Planning AGILE/DB/Script/test.sql
+++
@@ -1,7 +0,0 @@
-create table user(name varchar(50));
-
-insert into user values("Hardik");
-insert into user values("Kamana");
-insert into user values("Udit");
-insert into user values("Joy");
-insert into user values("Mayank");
