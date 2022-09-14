26c58bb09ea683d1f39d47537736baa6e25eb75e
joyPahari <joypahari11@gmail.com>
2022-09-14 12:49:17 +0530

    [joy] modified script

--- DB/Script/CreateScript.sql
+++ DB/Script/CreateScript.sql
@@ -1,13 +1,15 @@
 create database cdac_app_final;
 use cdac_app_final;
-----------------------------------------------------------------------------------------------------------------
--- Date format YYYY-MM-DD 
+
+-- ----------------------------------------------------------------------
+-- ccat_student
+
 create table if not exists ccat_student(
 	ccat_no bigint PRIMARY KEY NOT NULL,
     f_name varchar(25) NOT NULL, 
 	m_name varchar(25),
     l_name varchar(25),
-    dob date NOT NULL,
+    dob date NOT NULL,		-- Date format YYYY-MM-DD 
     course varchar(10) NOT NULL
 );
 
@@ -18,8 +20,7 @@ insert into ccat_student values('2203004','Mayank',NULL,'Sachan','1994-03-07','P
 insert into ccat_student values('2203005','Kamana',NULL,'Mathur','1997-07-04','PG-XYZ');
 insert into ccat_student values('1111111','admin','admin','admin','1999-01-01','PG-DAC');
 
--- ---------------------------------------------------------------------------------------------------------------------------------------------
-
+-- ----------------------------------------------------------------------
 -- user_table
 
 CREATE TABLE IF NOT EXISTS user_table(
@@ -31,15 +32,14 @@ CREATE TABLE IF NOT EXISTS user_table(
 
 INSERT INTO user_table VALUES (1, 'Hardik', 2203001, 'Student');
 
--- ----------------------------------------------------------------------------------------------------------------------------------------------
--- ------------------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------
 -- personal_table
--- Date format YYYY-MM-DD 
+
 create table if not exists personal_details(
 	u_id bigint PRIMARY KEY NOT NULL, f_name varchar(25) NOT NULL,
     m_name varchar(25), l_name varchar(25),
     gender varchar(1) NOT NULL,
-    dob date NOT NULL,
+    dob date NOT NULL,		-- Date format YYYY-MM-DD
     email varchar(60) NOT NULL,
     phone bigint NOT NULL,
     qualification varchar(15) NOT NULL,
@@ -47,7 +47,8 @@ create table if not exists personal_details(
 );
 
 insert into personal_details values('1','Hardik',NULL,'Agarwal','M','2000-03-17','hardik@gmail.com','8630296896','B.tech','https:///dcjcbbivfvbv');
--- ------------------------------------------------------------------------------------------------------
+
+-- ----------------------------------------------------------------------
 -- user_address
 
 CREATE TABLE IF NOT EXISTS user_address(
@@ -61,7 +62,7 @@ CREATE TABLE IF NOT EXISTS user_address(
                             
 INSERT INTO user_address VALUES(1,'Ganesh Apartment','Kisan Nagar','Uttar Pradesh','Hapur',400001);
                                 
------------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------
 -- student_user
 
 CREATE TABLE IF NOT EXISTS student_user(
@@ -72,7 +73,7 @@ CREATE TABLE IF NOT EXISTS student_user(
 
 INSERT INTO student_user VALUES('1','2203001','PG-DAC');
 
--- -----------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------
 -- guardian
 
 CREATE TABLE IF NOT EXISTS guardian(
@@ -84,8 +85,7 @@ CREATE TABLE IF NOT EXISTS guardian(
 
 INSERT INTO guardian VALUES(2203001, 1, 'Mala Agarwal', '1234567890');
 
--- ---------------------------------------------------------------------------------------
-
+-- ----------------------------------------------------------------------
 -- user_prn
 CREATE TABLE IF NOT EXISTS user_prn(u_id bigint primary key not null,
                       u_prn bigint not null,
@@ -93,8 +93,7 @@ CREATE TABLE IF NOT EXISTS user_prn(u_id bigint primary key not null,
                       
 INSERT INTO user_prn VALUES(1,220340120083,'220340120083');
 
--- ---------------------------------------------------------------------------------------
-
+-- ----------------------------------------------------------------------
 -- user_login
 CREATE TABLE IF NOT EXISTS user_login(u_id bigint primary key not null,
                         u_name bigint not null,
