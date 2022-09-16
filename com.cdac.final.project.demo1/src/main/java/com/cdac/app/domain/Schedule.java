fab2180930d1e6d2123dcdaa3afd2f213a246b86
joyPahari <joypahari11@gmail.com>
2022-09-16 14:19:55 +0530

    [joy] updated schedule pojo

--- com.cdac.final.project.demo1/src/main/java/com/cdac/app/domain/Schedule.java
+++ com.cdac.final.project.demo1/src/main/java/com/cdac/app/domain/Schedule.java
@@ -21,19 +21,32 @@ public class Schedule {
 	@Column(name = "lect_link")
 	private String lectLink;
 
-	@Column(name = "lab_link")
-	private String labLink;
+	@Column(name = "lab_b1_link")
+	private String labB1Link;
+
+	@Column(name = "lab_b2_link")
+	private String labB2Link;
+
+	@Column(name = "lab_b3_link")
+	private String labB3Link;
+
+	@Column(name = "lab_b4_link")
+	private String labB4Link;
 
 	public Schedule() {
 		super();
 	}
 
-	public Schedule(LocalDate date, String moduleName, String lectLink, String labLink) {
+	public Schedule(LocalDate date, String moduleName, String lectLink, String labB1Link, String labB2Link,
+			String labB3Link, String labB4Link) {
 		super();
 		this.date = date;
 		this.moduleName = moduleName;
 		this.lectLink = lectLink;
-		this.labLink = labLink;
+		this.labB1Link = labB1Link;
+		this.labB2Link = labB2Link;
+		this.labB3Link = labB3Link;
+		this.labB4Link = labB4Link;
 	}
 
 	public LocalDate getDate() {
@@ -60,18 +73,43 @@ public class Schedule {
 		this.lectLink = lectLink;
 	}
 
-	public String getLabLink() {
-		return labLink;
+	public String getLabB1Link() {
+		return labB1Link;
 	}
 
-	public void setLabLink(String labLink) {
-		this.labLink = labLink;
+	public void setLabB1Link(String labB1Link) {
+		this.labB1Link = labB1Link;
+	}
+
+	public String getLabB2Link() {
+		return labB2Link;
+	}
+
+	public void setLabB2Link(String labB2Link) {
+		this.labB2Link = labB2Link;
+	}
+
+	public String getLabB3Link() {
+		return labB3Link;
+	}
+
+	public void setLabB3Link(String labB3Link) {
+		this.labB3Link = labB3Link;
+	}
+
+	public String getLabB4Link() {
+		return labB4Link;
+	}
+
+	public void setLabB4Link(String labB4Link) {
+		this.labB4Link = labB4Link;
 	}
 
 	@Override
 	public String toString() {
-		return "Schedule [date=" + date + ", moduleName=" + moduleName + ", lectLink=" + lectLink + ", labLink="
-				+ labLink + "]";
+		return "Schedule [date=" + date + ", moduleName=" + moduleName + ", lectLink=" + lectLink + ", labB1Link="
+				+ labB1Link + ", labB2Link=" + labB2Link + ", labB3Link=" + labB3Link + ", labB4Link=" + labB4Link
+				+ "]";
 	}
 
 }
