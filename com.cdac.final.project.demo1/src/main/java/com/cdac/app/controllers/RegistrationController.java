420db71bc2a019351b55154036cc92f5b40b4e75
kamana1997 <104790201+kamana1997@users.noreply.github.com>
2022-09-18 11:52:04 +0530

    [kamana] updated personalDetails

--- com.cdac.final.project.demo1/src/main/java/com/cdac/app/controllers/RegistrationController.java
+++ com.cdac.final.project.demo1/src/main/java/com/cdac/app/controllers/RegistrationController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cdac.app.domain.Guardian;
 import com.cdac.app.domain.PersonalDetails;
 import com.cdac.app.domain.UserAddress;
 import com.cdac.app.service.IRegistrationService;
@@ -50,16 +49,7 @@ public class RegistrationController {
 	  UserAddress addressDetails = (UserAddress)o;
 	  service.saveAddressDetails(addressDetails);
 	}
-	
-	// API to save guardian details in database
-	// Input : json in form of object
-	@PostMapping("/saveGuardianDetails")
-	public void saveGuardianDetails(@PathVariable(name = "o") Object o) {
-		Guardian guardianDetails = (Guardian) o;
-		service.saveGuardianDetails(guardianDetails);
-	}
 
-	// 
 	@PostMapping("/generatePRN")
 	public void generatePRN(@PathVariable(name = "courseName") String courseName) {
 		service.generatePRN(courseName);
