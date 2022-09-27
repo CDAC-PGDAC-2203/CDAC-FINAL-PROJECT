abce6f2671af7f3957dfb22ec21692843bea14b0
joyPahari <joypahari11@gmail.com>
2022-09-27 17:52:31 +0530

    [joy] test cases added to script, old script flushed out

--- com.cdac.final.project.demo1/src/db/InsertScript.sql
+++ com.cdac.final.project.demo1/src/db/InsertScript.sql
@@ -6,7 +6,6 @@ insert into ccat_student values('2203003','Udit',NULL,'Keskar','1999-11-23','PG-
 insert into ccat_student values('2203004','Mayank',NULL,'Sachan','1994-03-07','PG-DBDA');
 insert into ccat_student values('2203005','Kamana',NULL,'Mathur','1997-07-04','PG-DESD');
 insert into ccat_student values('1111111','admin','admin','admin','1999-01-01','PG-DAC');
-insert into ccat_student values('1111112','admin','admin','admin','1999-01-01','PG-DAC');
 
 -- --------------------------------------------------------------------------------------------
 
@@ -15,7 +14,6 @@ insert into ccat_student values('1111112','admin','admin','admin','1999-01-01','
 INSERT INTO user_table VALUES (1, 'Hardik', 2203001, 'STUDENT');
 INSERT INTO user_table VALUES (2, 'Joy', 2203002, 'STUDENT');
 INSERT INTO user_table VALUES (10000, 'admin', 1111111, 'ADMIN');
-INSERT INTO user_table VALUES (10001, 'admin', 1111112, 'ADMIN');
 
 -- --------------------------------------------------------------------------------------------
 
@@ -33,32 +31,41 @@ INSERT INTO user_address VALUES(2,'Flat No. C/4','Navelkar Bldg, Mangoor Hill','
 -- --------------------------------------------------------------------------------------------
 
 -- user_login table
+
 insert into user_login values(10000,'admin',11111,'$2a$10$dp.US7K4f97owgHXpmwyUO1aiox8vexAFD/egkn4QHUDvqI4.NHn2','ADMIN','PG-DAC');
 
 -- --------------------------------------------------------------------------------------------
 
 -- module tables
 
-insert into module_1 values(220340120001,35,15,5);
-insert into module_2 values(220340120001,34,15,5);
-insert into module_3 values(220340120001,35,14,5);
-insert into module_4 values(220340120001,36,12,5);
+insert into module_1 values(220340120001,35,15,4);
+insert into module_2 values(220340120001,34,15,4);
+insert into module_3 values(220340120001,35,14,4);
+insert into module_4 values(220340120001,36,12,4);
+insert into module_1 values(220340120002,35,15,4);
+insert into module_2 values(220340120002,35,15,4);
+insert into module_3 values(220340120002,35,14,4);
+insert into module_4 values(220340120002,35,12,3);
 
 -- --------------------------------------------------------------------------------------------
 
 -- final_result table
 
-insert into final_result values(220340120001,50,40,50,40,60,null,null,null);
-insert into final_result values(220340120002,60,55,50,45,40,null,null,null);
+insert into final_result values(220340120001,54,53,53,52,null,null,null,null);
+insert into final_result values(220340120002,54,54,53,51,null,null,null,null);
 
 -- --------------------------------------------------------------------------------------------
 
 -- total_attendance table
 
 INSERT INTO total_attendance VALUES(220340120001,'mod1',8,10);
-INSERT INTO total_attendance VALUES(220340120001,'mod2',7,10);
-INSERT INTO total_attendance VALUES(220340120001,'mod3',2,10);
-INSERT INTO total_attendance VALUES(220340120001,'mod4',4,10);
+INSERT INTO total_attendance VALUES(220340120001,'mod2',8,10);
+INSERT INTO total_attendance VALUES(220340120001,'mod3',8,10);
+INSERT INTO total_attendance VALUES(220340120001,'mod4',8,10);
+INSERT INTO total_attendance VALUES(220340120002,'mod1',8,10);
+INSERT INTO total_attendance VALUES(220340120002,'mod2',8,10);
+INSERT INTO total_attendance VALUES(220340120002,'mod3',8,10);
+INSERT INTO total_attendance VALUES(220340120002,'mod4',6,10);
 
 -- --------------------------------------------------------------------------------------------
 
@@ -80,17 +87,16 @@ INSERT INTO feedback VALUES(220340120002,'PG-DAC','Madhura','mod1',3,4,5,5,5,20,
 
 -- faculty table
 
-INSERT INTO faculty VALUES(1,'A','N','PG-DAC');
-INSERT INTO faculty VALUES(2,'B','N','PG-XYZ');
+INSERT INTO faculty VALUES(1,'Madhura','Y','PG-DAC');
+INSERT INTO faculty VALUES(2,'B','N','PG-DESD');
 INSERT INTO faculty VALUES(3,'C','N','PG-DAC');
 
 -- --------------------------------------------------------------------------------------------
 
 -- notice table
 
-INSERT INTO notice VALUES('ABCD','http://link1','N');
-INSERT INTO notice VALUES('EFGH','http://link2','N');
-INSERT INTO notice VALUES('IJKL','http://link3','N');
+INSERT INTO notice VALUES('Naukri Alerts !!!','https://www.naukri.com/hiring-jobs','Y');
+INSERT INTO notice VALUES('TCS Hiring!!!','https://www.tcs.com/careers/entry-level','Y');
 
 -- --------------------------------------------------------------------------------------------
 
