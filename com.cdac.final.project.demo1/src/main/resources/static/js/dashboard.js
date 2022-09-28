875e5738cbb46fd4066011d2b2ff68aeac2b3275
uk2311 <keskarudit@gmail.com>
2022-09-28 11:42:54 +0530

    [udit] Progress Bar Completely Fixed

--- com.cdac.final.project.demo1/src/main/resources/static/js/dashboard.js
+++ com.cdac.final.project.demo1/src/main/resources/static/js/dashboard.js
@@ -9,8 +9,8 @@ $(document).ready(() => {
         reader.readAsDataURL(this.files[0]);
     });
 
-    var totalPerformance = 0.0;
-    var totalAttendance = 0.0;
+   		 var totalPerformance = 0;
+    	 var totalAttendance = 0;
 
     // AJAX to get current performance of student
     $.ajax({
@@ -19,37 +19,42 @@ $(document).ready(() => {
         beforeSend: function (xhr) { xhr.setRequestHeader('Authorization', localStorage.getItem("token")) },
         success: (data) => {
             totalPerformance = parseFloat(data.toFixed(1));
+			    if (totalPerformance > 0.0) {
+			    // Circular Progress Bar : Current Performance
+			        var progressBar1 = document.querySelector(".circular-progress-1");
+			        var valueContainer1 = document.querySelector(".value-container-1");
+			
+			        var progressValue1 = 0.0;
+			        progressValue1 = parseFloat(parseFloat(progressValue1).toFixed(1));
+			        var progressEndValue1 = parseFloat((totalPerformance).toFixed(1));
+			        var speed1 = 0.1;
+			
+			        var progress1 = setInterval(() => {
+			            progressValue1 = parseFloat((progressValue1 + 0.1).toFixed(1));
+			            valueContainer1.textContent = `${progressValue1}%`;
+			            progressBar1.style.background = `conic-gradient(
+			                #804FB3 ${progressValue1 * 0.6}deg,
+			                #9969C7 ${progressValue1 * 1.2}deg,
+			                #e91e63 ${progressValue1 * 2.7}deg,
+			                white ${progressValue1 * 3.6}deg
+			            )`;
+			            console.log("progressValue1",parseFloat((progressValue1).toFixed(1)));
+			            console.log("progressEndValue1",parseFloat((progressEndValue1).toFixed(1)));
+			            if (parseFloat((progressValue1).toFixed(1)) == parseFloat((progressEndValue1).toFixed(1))) {
+			                clearInterval(progress1);
+			            }
+			        }, speed1);
+			    } else {
+			    	var valueContainer1 = document.querySelector(".value-container-1");
+			    	valueContainer1 = `0%`;
+			    }
         },
         error: (error) => {
             alert("Internal Server Error!");
         }
     });
-    if (totalPerformance != 0) {
-        // Circular Progress Bar : Current Performance
-        var progressBar1 = document.querySelector(".circular-progress-1");
-        var valueContainer1 = document.querySelector(".value-container-1");
-
-        var progressValue1 = 0.0;
-        progressValue1 = parseFloat(parseFloat(progressValue1).toFixed(1));
-        var progressEndValue1 = parseFloat((totalPerformance).toFixed(1));
-        var speed1 = 0.1;
-
-        var progress1 = setInterval(() => {
-            progressValue1 = parseFloat((progressValue1 + 0.1).toFixed(1));
-            valueContainer1.textContent = `${progressValue1}%`;
-            progressBar1.style.background = `conic-gradient(
-                #804FB3 ${progressValue1 * 0.6}deg,
-                #9969C7 ${progressValue1 * 1.2}deg,
-                #e91e63 ${progressValue1 * 2.7}deg,
-                white ${progressValue1 * 3.6}deg
-            )`;
-            if (parseFloat((progressValue1).toFixed(1)) == parseFloat((progressEndValue1).toFixed(1))) {
-                clearInterval(progress1);
-            }
-        }, speed1);
-    }
-
-
+    
+	
     // AJAX to get current attendance of student
     $.ajax({
         type: "GET",
@@ -57,36 +62,39 @@ $(document).ready(() => {
         beforeSend: function (xhr) { xhr.setRequestHeader('Authorization', localStorage.getItem("token")) },
         success: (data) => {
             totalAttendance = parseFloat(data.toFixed(1));
+            if (totalAttendance > 0.0) {
+			    // Circular Progress Bar : Overall Attendace
+			        var progressBar2 = document.querySelector(".circular-progress-2");
+			        var valueContainer2 = document.querySelector(".value-container-2");
+			
+			        var progressValue2 = 0.0;
+			        progressValue2 = parseFloat(parseFloat(progressValue2).toFixed(1));
+			        var progressEndValue2 = parseFloat((totalAttendance).toFixed(1));
+			        var speed2 = 0.5;
+			
+			        var progress2 = setInterval(() => {
+			            progressValue2 = parseFloat((progressValue2 + 0.1).toFixed(1));
+			            valueContainer2.textContent = `${progressValue2}%`;
+			            progressBar2.style.background = `conic-gradient(
+			                #804FB3 ${progressValue2 * 0.6}deg,
+			                #9969C7 ${progressValue2 * 1.2}deg,
+			                #e91e63 ${progressValue2 * 2.7}deg,
+			                white ${progressValue2 * 3.6}deg
+			            )`;
+			            if (parseFloat((progressValue2).toFixed(1)) == parseFloat((progressEndValue2).toFixed(1))) {
+			                clearInterval(progress2);
+			            }
+			        }, speed2);
+				   } else {
+				   	var progressBar2 = document.querySelector(".circular-progress-2");
+				    var valueContainer2 = document.querySelector(".value-container-2");
+				   	valueContainer2.textContent = `0%`;
+			    }
         },
         error: (error) => {
             alert("Internal Server Error!");
         }
     });
-
-    if (totalAttendance != 0) {
-        // Circular Progress Bar : Overall Attendace
-        var progressBar2 = document.querySelector(".circular-progress-2");
-        var valueContainer2 = document.querySelector(".value-container-2");
-
-        var progressValue2 = 0.0;
-        progressValue2 = parseFloat(parseFloat(progressValue2).toFixed(1));
-        var progressEndValue2 = parseFloat((totalAttendance).toFixed(1));
-        var speed2 = 0.5;
-
-        var progress2 = setInterval(() => {
-            progressValue2 = parseFloat((progressValue2 + 0.1).toFixed(1));
-            valueContainer2.textContent = `${progressValue2}%`;
-            progressBar2.style.background = `conic-gradient(
-                #804FB3 ${progressValue2 * 0.6}deg,
-                #9969C7 ${progressValue2 * 1.2}deg,
-                #e91e63 ${progressValue2 * 2.7}deg,
-                white ${progressValue2 * 3.6}deg
-            )`;
-            if (parseFloat((progressValue2).toFixed(1)) == parseFloat((progressEndValue2).toFixed(1))) {
-                clearInterval(progress2);
-            }
-        }, speed2);
-    }
 
     //AJAX to get module wise attendance and draw the bar graph.
     $.ajax({
@@ -201,6 +209,10 @@ $(document).ready(() => {
             alert("Internal Server Error! Please contact Administrator");
         }
     });
+    
+    
+    
+    
 
     // Fetch Module List for drop down in doubt widget
     var courseModules = "";
